20177ed9559a949b61aa3cf955a66f6b4479b870
Leonard Luangga Kekang <leonard.luangga@gmail.com>
2011-12-31 10:58:27 +0800

    Update 85touchscreen.sh

--- 85touchscreen.sh
+++ 85touchscreen.sh
@@ -9,3 +9,4 @@ echo 8002 > /sys/class/touch/switch/set_touchscreen;
 echo 11000 > /sys/class/touch/switch/set_touchscreen;
 echo 13060 > /sys/class/touch/switch/set_touchscreen;
 echo 14005 > /sys/class/touch/switch/set_touchscreen;
+
