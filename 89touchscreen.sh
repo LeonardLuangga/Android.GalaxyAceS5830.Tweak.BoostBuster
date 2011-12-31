e8be40e4fa8073b9ab8856c84aa5700b0846b243
Leonard Luangga Kekang <leonard.luangga@gmail.com>
2011-12-31 09:20:27 +0800

    Update 89touchscreen.sh

---
+++ 89touchscreen.sh
@@ -0,0 +1,11 @@
+#!/system/bih/sh
+#
+# from V8 TurboBoost by GadgetCheck
+
+
+#configure touchscreen sensitivity
+echo 7035 > /sys/class/touch/switch/set_touchscreen;
+echo 8002 > /sys/class/touch/switch/set_touchscreen;
+echo 11000 > /sys/class/touch/switch/set_touchscreen;
+echo 13060 > /sys/class/touch/switch/set_touchscreen;
+echo 14005 > /sys/class/touch/switch/set_touchscreen;
